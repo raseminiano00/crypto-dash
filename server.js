b516e88980b1f9a95b31a01fa37db49f712c3843
roi <raseminiano00@gmail.com>
2021-10-24 18:28:27 +0800

    Rescript server.js for heroku deployment

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/crpyto-dashboard'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: '/dist/crpyto-dashboard'}),
-);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
