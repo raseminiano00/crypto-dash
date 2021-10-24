bfb24c360d9be35520fbebfa4d6bf485a0790435
roi <raseminiano00@gmail.com>
2021-10-24 18:34:25 +0800

    Add missing route on server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/crpyto-dashboard'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(__dirname + '/dist/crpyto-dashboard/index.html');
 });
 
 // Start the app by listening on the default Heroku port
